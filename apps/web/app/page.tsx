8e21297f82510d609224a0bf8107de1195e6a2fb
sk_883 <shivamkumar.sps2004@gmail.com>
2025-04-12 22:40:43 +0530

    docker tailwind common and ui added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -133,6 +133,7 @@ export default function Page() {
           </Card>
         ))}
       </div>
+      <div className="text-xl">odfhbhsdf</div>
     </main>
   );
 }
--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,31 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+import path from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"], // add more shared packages here if needed
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // use with caution in prod
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // disable linting in CI builds
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
